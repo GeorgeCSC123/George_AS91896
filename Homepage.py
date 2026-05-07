9a149d2e1fc5e826146aa2e55ade41721ccb4020
GeorgeCSC123 <23068@students.mrgs.school.nz>
2026-05-07 13:19:23 +1200

    Refactor Homepage.py to improve label handling

--- Homepage.py
+++ Homepage.py
@@ -5,11 +5,12 @@ root = tk.Tk()
 root.title("My App")
 root.geometry("400x300")
 root.configure(bg="#7ed957")
-label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial", 20, "bold"))
-my_image = tk.PhotoImage(file="image1.png")
-label = tk.Label(root, image=my_image)
-label.pack(pady=20)
+title_label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial", 20, "bold"))
 
+photo = tk.PhotoImage(file="image1.png")
+photo_label = tk.Label(root, image=photo)
+
+title_label.pack(pady=20)
+photo_label.pack(pady=20)
 
 root.mainloop()
-
