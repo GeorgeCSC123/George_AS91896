64145e5f514a4bf896de0f76e39da5185e2bae0c
GeorgeCSC123 <23068@students.mrgs.school.nz>
2026-05-12 09:51:57 +1200

    Refactor Homepage.py for consistency and clarity

--- Homepage.py
+++ Homepage.py
@@ -5,13 +5,14 @@ root = tk.Tk()
 root.title("My App")
 root.geometry("400x300")
 root.configure(bg="#7ed957")
+
 title_label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial", 20, "bold"))
 
 photo = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-11 101835.png")
-photo_label = tk.Label(root, image=photo)
+photo_label = tk.Label(root, image=photo, bg="#7ed957")
 
 photo2 = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-11 103650.png")
-photo2_label = tk.Label(root, image=photo2)
+photo2_label = tk.Label(root, image=photo2, bg="#7ed957")
 
 
 title_label.pack(pady=20)
