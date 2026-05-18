1834ed5dd85248e4c8d51cd9899bcd90ae07ce79
GeorgeCSC123 <23068@students.mrgs.school.nz>
2026-05-19 09:20:31 +1200

    Change image paths in Homepage.py
    Updated image file paths for photo and photo2 labels.

--- Homepage.py
+++ Homepage.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 root = tk.Tk()
 root.title("My App")
 root.geometry("400x300")
@@ -8,10 +7,10 @@ root.configure(bg="#7ed957")
 
 title_label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial", 20, "bold"))
 
-photo = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-11 101835.png")
+photo = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-19 091556.png")
 photo_label = tk.Label(root, image=photo, bg="#7ed957")
 
-photo2 = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-11 103650.png")
+photo2 = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-19 091833.png")
 photo2_label = tk.Label(root, image=photo2, bg="#7ed957")
 
 
