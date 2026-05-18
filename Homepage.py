4dd16066fff3d1a285f368239d50ac04a3c32790
GeorgeCSC123 <23068@students.mrgs.school.nz>
2026-05-19 09:43:31 +1200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("My App")
 root.geometry("400x300")
 root.configure(bg="#7ed957")
 
-title_label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial", 20, "bold"))
+title_label = tk.Label(root, text="George’s countries quiz", fg="white", bg="#7ed957", font=("arial",30, "bold"))
 
 photo = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-19 091556.png")
 photo_label = tk.Label(root, image=photo, bg="#7ed957")
@@ -14,8 +15,8 @@ photo2 = tk.PhotoImage(file=r"C:\Users\relia\Downloads\12CSC\Screenshot 2026-05-
 photo2_label = tk.Label(root, image=photo2, bg="#7ed957")
 
 
-title_label.pack(pady=20)
-photo_label.pack(pady=20)
-photo2_label.pack(pady=20)
+title_label.pack(pady=40)
+photo_label.pack(pady=40)
+photo2_label.pack(pady=40)
 
 root.mainloop()
