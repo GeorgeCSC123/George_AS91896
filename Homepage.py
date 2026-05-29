fdda13b3776db081e00825c1a7a35be461cd9587
GeorgeCSC123 <23068@students.mrgs.school.nz>
2026-05-29 12:32:36 +1200

    Enhance name entry with placeholder and clear function
    Added functionality to clear the name entry field when focused and set a placeholder text.

--- Homepage.py
+++ Homepage.py
@@ -7,10 +7,16 @@ def submit_name():
 def start_quiz():
     print("Quiz started!")
 
+# clear text
+def clear(event):
+    if name_entry.get() == "Enter name to start":
+        name_entry.delete(0, tk.END)
+
+
 # main window
 root = tk.Tk()
 root.title("My App")
-root.geometry("900x700")
+root.geometry("950x700")
 root.configure(bg="#7ed957")
 
 # title
@@ -24,13 +30,21 @@ photo_label = tk.Label(root, image=photo, bg="#7ed957")
 photo2 = tk.PhotoImage(file=r"C:\Users\23068\OneDrive - Mt Roskill Grammar School\Documents\Vscode y12\Screenshot 2026-05-27 142520.png")
 
 # image button
-start_button = tk.Button(root, image=photo2, command=start_quiz, bd=0, relief="flat", highlightthickness=0, bg="#7ed957", activebackground="#7ed957", cursor="hand2")
+start_button = tk.Button(root, image=photo2, command=start_quiz, bd=0, relief="flat", highlightthickness=0, bg="#7ed957",
+                         activebackground="#7ed957", cursor="hand2")
 
 #  name entry box
-name_entry = tk.Entry(root, font=("Arial", 40), bg="white", fg="black", bd=2, relief="solid", justify="center")
+name_entry = tk.Entry(root, font=("Arial", 35), bg="white", fg="black", bd=2, relief="solid", justify="center")
+
+# enter name text
+name_entry.insert(0, "Enter name to start")
+
+# clears the text
+name_entry.bind("<FocusIn>", clear)
 
 # submit button
-submit_button = tk.Button(root, text="Submit Name", command=submit_name, font=("Arial", 35, "bold"), fg="white", bg="black", activebackground="gray20", activeforeground="white", cursor="hand2")
+submit_button = tk.Button(root, text="Submit Name", command=submit_name, font=("Arial", 30, "bold"), fg="white", bg="black",
+                          activebackground="gray20", activeforeground="white", cursor="hand2")
 
 # positioning of widgets
 title_label.pack(pady=10)
